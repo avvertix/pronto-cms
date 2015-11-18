0ee589172568a60278ee512dd5c7d03f3237ce7d
alessio <alessio.vertemati@gmail.com>
2015-11-18 20:42:17 +0100

    restyle to make it like section.blade.php

--- resources/views/default/page.blade.php
+++ resources/views/default/page.blade.php
@@ -2,19 +2,17 @@
 
 @section('main')
 
-<div class="container">
+<div class="container grid grid--gutters">
 
-<div class="sidebar">
+<div class="sidebar grid-cell u-1of3">
 	@include('partials.section_navigation')
 </div>
 
-<div class="page-content">
+<div class="page-content grid-cell">
 
 	@if(isset($content))
 		{!! $content !!}
 	@endif
-
-<pre><code>{{var_dump($navigation)}}</code></pre>
 	
 </div>
 	
